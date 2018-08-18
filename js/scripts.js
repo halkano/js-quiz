53de2bc8f5ceaf7dfb057478b5b7a5da5573dffb
Maryann Gitonga <naygitonga4@gmail.com>
2018-08-18 13:07:28 +0300

    Add back-end logic

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Back-end logic
+var inputedAnswers = [],
+    scoredPoints = 0;
+    questions = ["ans-1", "ans-2", "ans-3", "ans-4"];
+
+function pointsCompiler(){
+  scoredPoints = inputedAnswers[0] + inputedAnswers[1] + inputedAnswers[2] + inputedAnswers[3];
+  return points;
+}
