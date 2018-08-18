88638246c4dbfe10bf681b92e433f8a279758906
Maryann Gitonga <naygitonga4@gmail.com>
2018-08-18 13:09:19 +0300

    Add user-interface logic, all buttons link to the right ids and submit button able to calculate points

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,94 @@ function pointsCompiler(){
   scoredPoints = inputedAnswers[0] + inputedAnswers[1] + inputedAnswers[2] + inputedAnswers[3];
   return points;
 }
+// Front-end logic
+$(document).ready(function(){
+  $(".question").hide();
+  $("#q-1").show();
+
+  $("#btn-1").click(function(){
+    if(isNaN($("input:radio[name = ans-1]:checked").val())){
+      alert("Please answer question 1 to proceed!");
+      $(".question").hide();
+      $("#q-1").show();
+      return false;
+    }else{
+      $(".question").hide();
+      $("#q-1").hide();
+      $("#q-2").show();
+      return false;
+    }
+  });
+
+  $("#btn-2-1").click(function(){
+    $(".question").hide();
+    $("#q-2").hide();
+    $("#q-1").show();
+    return false;
+  });
+  $("#btn-2-2").click(function(){
+    if(isNaN($("input:radio[name = ans-2]:checked").val())){
+      alert("Please answer question 2 to proceed!");
+      $(".question").hide();
+      $("#q-1").hide();
+      $("#q-2").show();
+      return false;
+    }else{
+      $(".question").hide();
+      $("#q-2").hide();
+      $("#q-3").show();
+      return false;
+    }
+  });
+
+  $("#btn-3-1").click(function(){
+    $(".question").hide();
+    $("#q-3").hide();
+    $("#q-2").show();
+    return false;
+  });
+  $("#btn-3-2").click(function(){
+    if(isNaN($("input:radio[name = ans-3]:checked").val())){
+      alert("Please answer question 3 to proceed!");
+      $(".question").hide();
+      $("#q-2").hide();
+      $("#q-3").show();
+      return false;
+    }else{
+      $(".question").hide();
+      $("#q-3").hide();
+      $("#q-4").show();
+      return false;
+    }
+  });
+
+  $("#btn-4-1").click(function(){
+    $(".question").hide();
+    $("#q-4").hide();
+    $("#q-3").show();
+    return false;
+  });
+
+  $("#btn-4-2").click(function(){
+    if(isNaN($("input:radio[name = ans-4]:checked").val())){
+      alert("Please answer question 4 to submit your answers!");
+      $(".question").hide();
+      $("#q-3").hide();
+      $("#q-4").show();
+      return false;
+    }else{
+      $("#fm-1").submit(function(event){
+        event.preventDefault();
+        questions.forEach(function(question){
+          inputedAnswers.push(parseInt($("input:radio[name=" + question + "]:checked").val()));
+        });
+        pointsCompiler();
+
+        $("#test").hide();
+        $("#q-4").hide();
+        $("#output").show();
+        $("#points").append(scoredPoints);
+      });
+    }
+  });
+});
